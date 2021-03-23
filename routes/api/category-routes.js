6c29ac61b6f4a641490ddb760517e72f2ba98813
Matthew Webb <mwebb2321@gmail.com>
2021-03-22 18:43:39 -0700

    added to get one category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,7 +21,20 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: {
+      model: Product,
+      attributes: ["category_id"],
+    },
+  })
+    // be sure to include its associated Products
+    .then((categoryData) => res.json(categoryData))
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 router.post("/", (req, res) => {
