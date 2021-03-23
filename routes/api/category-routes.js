d53699e2cf8c5e44244d7f23f11d0e72bb2a34d4
Matthew Webb <mwebb2321@gmail.com>
2021-03-23 15:17:11 -0700

    reworking cat-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,36 +3,41 @@ const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
-  Category.findAll({
-    include: {
-      model: Product,
-      attributes: ["product_name"],
-    },
-  })
-    .then((categoryData) => res.json(categoryData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+  try {
+    const categoryData = await Category.findAll({
+      include: {
+        model: Product,
+        attributes: ["product_name"],
+      },
     });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
-  Category.findOne({
-    where: {
-      id: req.params.id,
-    },
-    include: {
+  try {
+    const categoryData = await Category.findByPk(req.params.id {
+    include: [
+      {
       model: Product,
       attributes: ["category_id"],
     },
-  })
-    .then((categoryData) => res.json(categoryData))
-    .catch((err) => {
-      res.status(500).json(err);
-    });
+    ],
+  });
+  if (!categoryData) {
+    res.status(404).json({ message: 'No category found with this id!' });
+    return;
+  };
+  res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 router.post("/", (req, res) => {
