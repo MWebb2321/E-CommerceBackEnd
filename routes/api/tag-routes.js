d4e8db5e1140cbb712c77dd73a7d65b76f264824
Matthew Webb <mwebb2321@gmail.com>
2021-03-23 13:39:42 -0700

    tag update

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -50,6 +50,27 @@ router.post("/", (req, res) => {
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((tagData) => {
+      if (!tagData) {
+        res.stats(404).json({ message: "No Tag found with that ID." });
+        return;
+      }
+      res.json(tagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.stats(500).json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
