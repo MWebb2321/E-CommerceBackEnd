93903afd0013460375a6e3e791f68519e07328fd
Matthew Webb <mwebb2321@gmail.com>
2021-03-23 15:27:47 -0700

    taking a break

--- models/index.js
+++ models/index.js
@@ -12,18 +12,25 @@ Product.belongsTo(Category, {
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
+  onDelete: "Cascade",
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: "product_id",
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "tagged_products",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: "tag_id",
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "product_tags",
 });
 
 module.exports = {
