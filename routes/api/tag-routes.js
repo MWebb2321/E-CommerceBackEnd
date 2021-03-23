8f2fc049edde61e5e9173c9e91d9c1df0cc8f19c
Matthew Webb <mwebb2321@gmail.com>
2021-03-23 13:43:27 -0700

    tag delete and finished tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -75,6 +75,22 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tagData) => {
+      if (!tagData) {
+        res.stats(404).json({ message: "No Tag found with that ID." });
+        return;
+      }
+      res.json(tagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
